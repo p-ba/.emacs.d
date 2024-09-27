cd1da433fcf575bc40e2a030891691fd1dd08fdb
Pavel Baraulya <pbaraulya@gmail.com>
2024-09-27 19:45:34 +0200

    Disable evil mode :-O

--- early-init.el
+++ early-init.el
@@ -31,7 +31,3 @@
 (auto-compile-on-save-mode)
 
 (setq frame-inhibit-implied-resize t)
-
-(menu-bar-mode   -1)
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
--- init.el
+++ init.el
@@ -1,30 +1,44 @@
 (add-to-list 'load-path (concat user-emacs-directory "my-packages/"))
 
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
 (electric-indent-mode 1)
 (global-auto-revert-mode 1)
+(global-superword-mode t)
+
+(setq-default
+  cursor-in-non-selected-windows nil)
 
 (setq
  use-short-answers t
- y-or-n-p-use-read-key t 
+ y-or-n-p-use-read-key t
  ring-bell-function 'ignore
  scroll-preserve-screen-position t
  inhibit-startup-screen t
+ kill-whole-line 1
  custom-file "~/.emacs.d/init-custom.el"
- backup-directory-alist '(("." . "~/.emacs.d/backups"))
- auto-save-file-name-transforms `((".*" "~/.emacs.d/saves" t)))
+ save-files-directory (expand-file-name "backups/" user-emacs-directory)
+ auto-save-file-name-transforms `((".*" ,save-files-directory t))
+ backup-directory-alist `(("." . ,save-files-directory)))
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (setq package-native-compile t
+	  package-archives '(("nongnu" . "https://elpa.nongnu.org/nongnu/")
+                           ("gnu" . "https://elpa.gnu.org/packages/")
+                           ("melpa" . "https://melpa.org/packages/")
+                           ("devel" . "https://elpa.gnu.org/devel/"))
+      package-archive-priorities '(("devel" . -1))
       native-comp-async-report-warnings-errors nil)
-(package-initialize)
 
+(use-package nano
+  :ensure t
+  :vc (:url "https://github.com/rougier/nano-emacs" :branch "master"))
+
+;; OSX-specific
 (setq mac-option-key-is-meta nil
       mac-command-key-is-meta t
       mac-command-modifier 'meta
       mac-option-modifier 'super)
-(global-set-key (kbd "s-SPC") 'set-mark-command)
-(setq superword-mode 1)
+(global-set-key (kbd "s-;") 'pop-to-mark-command)
 
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
@@ -35,14 +49,41 @@
   (package-vc-install "https://github.com/emacs-php/php-ts-mode"))
 
 (use-package emacs
+  :hook
+  (before-save . delete-trailing-whitespace)
+  :custom
+  (set-mark-command-repeat-pop t)
+  (enable-recursive-minibuffers t)
+  (text-mode-ispell-word-completion nil)
+  (read-extended-command-predicate #'command-completion-default-include-p)
   :config
-  (set-face-attribute 'default nil :font "Iosevka Comfy" :height 170))
+  (setq require-final-newline t)
+  (set-face-attribute 'default nil :font "Iosevka Comfy" :height 170)
+  (advice-add 'pop-to-mark-command :around
+            (lambda (pop-to-mark &rest args)
+              (let ((p (point)))
+                (dotimes (_ 5)
+                  (when (= p (point))
+                    (apply pop-to-mark args)))))))
 
-(use-package organic-green-theme
-  :init (progn (load-theme 'organic-green t)
-               (enable-theme 'organic-green))
-  :defer t
-  :ensure t)
+(use-package ace-jump-mode
+  :ensure t
+  :config
+  (global-set-key (kbd "C-;") 'ace-jump-mode))
+
+(use-package dired
+  :custom
+  (setq insert-directory-program "gls")
+  (dired-use-ls-dired t)
+  (dired-recursive-copies 'always)
+  (dired-auto-revert-buffer t)
+  (dired-listing-switches "-Alhv --group-directories-first"))
+
+;; (use-package organic-green-theme
+;;   :init (progn (load-theme 'organic-green t)
+;;                (enable-theme 'organic-green))
+;;   :defer t
+;;   :ensure t)
 
 (use-package dumb-jump
   :ensure t
@@ -50,11 +91,12 @@
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
   (setq dumb-jump-force-searcher 'rg))
 
-(defun prog-mode-init ()
+(defun prog-mode-config()
   (setq display-line-numbers-type 'relative)
   (display-line-numbers-mode)
-  (setq tab-width 4))
-(add-hook 'prog-mode-hook 'prog-mode-init)
+  (setq tab-width 4)
+  (indent-tabs-mode  nil))
+(add-hook 'prog-mode-hook 'prog-mode-config)
 
 (use-package treesit-auto
   :ensure t
@@ -91,12 +133,40 @@
   :config
   (add-hook 'php-ts-mode-hook 'detect-web-mode))
 
-(require 'php-find-use)
+(use-package php-find-use
+  :config
+  (global-set-key (kbd "C-c f u") 'php-find-use))
 
+(use-package lsp-mode
+  :custom
+  (lsp-enable-symbol-highlighting nil)
+  (lsp-completion-provider :none)
+  (lsp-enable-file-watchers nil)
+  (lsp-diagnostics-mode -1)
+  :init
+  (defun my/lsp-mode-setup-completion ()
+    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+          '(orderless)))
+  :hook
+  (lsp-completion-mode . my/lsp-mode-setup-completion)
+  :ensure t)
+
+(defvar-keymap leader-commands
+  :doc "Commands for projects"
+  "f" 'project-find-file
+  "p" 'project-switch-project
+  "g" 'rg-project
+  "r" 'consult-imenu
+  "G" 'rg-all
+  "u" 'php-find-use)
+
+(keymap-set global-map "C-q" leader-commands)
+
+(setq evil-want-keybinding nil)
 (use-package evil
   :ensure t
   :config
-  (evil-mode 1)
+  ;; (evil-mode 1)
   (defun forward-evil-word (&optional count)
 	"Overrides 'forward-evil-word' from 'evil-common.el'
      Respects superword-mode setting"
@@ -117,25 +187,26 @@
 
   (global-unset-key (kbd "C-u"))
   (evil-define-key 'normal 'global (kbd "C-u") 'scroll-down)
-  
-  (evil-set-leader nil (kbd "SPC"))
-  (evil-define-key 'normal 'global (kbd "<leader>p") 'project-switch-project)
-  (evil-define-key 'normal 'global (kbd "<leader>f") 'project-find-file)
-  (evil-define-key 'normal 'global (kbd "<leader>g") 'rg-project)
-  (evil-define-key 'normal 'global (kbd "<leader>G") 'rg-all)
-  (evil-define-key 'normal 'global (kbd "<leader>r") 'consult-imenu)
-  (evil-define-key 'normal 'global (kbd "<leader>b") 'switch-to-buffer)
-  (evil-define-key 'normal 'global (kbd "<leader>u") 'universal-argument)
-  (evil-define-key 'normal 'global (kbd "<leader>s") 'save-buffer)
-  (evil-define-key 'normal 'global (kbd "<leader>i") 'php-find-use)
+  ;; (use-package evil-collection
+  ;; 	:ensure t
+  ;; 	:config
+  ;; 	(evil-collection-init))
 
-  (dolist (initial-states '((xref--xref-buffer-mode . emacs)
-							(occur-mode . emacs)
-							(vterm-mode . emacs)
-							(shell-mode . emacs)
-							(rg-mode . emacs)
-							(diff-mode . emacs)))
-	(evil-set-initial-state (car initial-states) (cdr initial-states))))
+  (evil-set-leader nil (kbd "SPC"))
+  (dolist (keymap leader-commands)
+	(when (consp keymap)
+	  (setq key (car keymap))
+	  (when key
+		(evil-define-key 'normal 'global (kbd (format "<leader>%c" key)) (cdr keymap))))))
+
+;;   ;; (dolist (initial-states '((xref--xref-buffer-mode . emacs)
+  ;; 							(occur-mode . emacs)
+  ;; 							(shell-mode . emacs)
+  ;; 							(rg-mode . emacs)
+  ;; 							(diff-mode . emacs)))
+  ;; 	(evil-set-initial-state (car initial-states) (cdr initial-states))))
+
+
 
 (use-package dabbrev
   ;; Swap M-/ and C-M-/
@@ -143,20 +214,27 @@
          ("C-M-/" . dabbrev-expand))
   :config
   (setq dabbrev-case-fold-search nil
-		dabbrev-case-replace nil))
+		dabbrev-case-replace nil)
+  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
 
 (use-package corfu
   :ensure t
   :init
   (global-corfu-mode))
 
-(use-package vterm
-  :ensure t
+(use-package consult
+  :custom
+  (consult-async-min-input 1)
   :ensure t)
 
-(use-package multi-vterm
+(use-package orderless
   :ensure t
-  :after vterm)
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package undo-tree
   :ensure t
@@ -169,6 +247,15 @@
   :config
   (vertico-mode))
 
+(use-package marginalia
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
+
+(use-package magit
+  :ensure t)
+
 (use-package editorconfig
   :ensure t
   :config
@@ -176,5 +263,12 @@
 
 (use-package wgrep
   :ensure t)
+
+(use-package markdown-mode
+  :ensure t
+  :mode (("\\.md$" . gfm-mode))
+  :commands gfm-mode
+  :bind (:map markdown-mode-map ("C-c l" . slot/often-used-links))
+  :custom (markdown-command "pandoc --standalone --mathjax --from=markdown"))
 
 (load custom-file 'noerror 'nomessage)
